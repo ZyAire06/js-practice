f80ba7590b2b98cd45149dfbb719361db51737ea
Zy'Aire McCreary-Scott <zyaire.mccrearyscott@gmail.com>
2022-10-17 14:35:38 -0400

    add exercise-1.js

---
+++ exercise-1.js
@@ -0,0 +1,8 @@
+let idNumber= null;
+let fName= "Zy'Aire";
+let mName="Monuqie";
+let lName= "McCreary-Scott";
+let gender= "female";
+let birthDate= 06/20/2001;
+let licenceNumber= null;
+let policyNumber= null;
