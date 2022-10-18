13e03783c6dc7a6a6136a236dff516e83762e4a8
Zy'Aire McCreary-Scott <zyaire.mccrearyscott@gmail.com>
2022-10-18 09:13:48 -0400

    add math.js

---
+++ math.js
@@ -0,0 +1,14 @@
+let tacoPrice=6.00; 
+console.log(typeof tacoPrice);
+
+let coupon = 3.00;
+
+let tacoQuantity = 3;
+
+let totalTocaPrice= tacoQuantity*tacoPrice;
+
+console.log(totalTocaPrice);
+
+let totalPrice= totalTocaPrice/ coupon;
+
+console.log(totalPrice);
