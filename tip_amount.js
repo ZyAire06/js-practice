db53171a63921f565141e78914cf57343635e753
Zy'Aire McCreary-Scott <zyaire.mccrearyscott@gmail.com>
2022-10-18 10:19:26 -0400

    add tip-amout html and js

---
+++ tip_amount.js
@@ -0,0 +1,11 @@
+// The tip on a $some-number food bill is $some-number
+let foodPrice= 33.00;
+
+let tipAmount= 33 *.2;
+
+let totalAmount= foodPrice + tipAmount;
+
+console.log(totalAmount);
+
+
+
